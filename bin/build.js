3f51b105199a0b8e2a450b4d93a0e782b7260980
Anthony Meizoso <anthony.me@protonmail.ch>
2022-08-19 15:03:49 +0200

    Removed script

--- bin/build.js
+++ bin/build.js
@@ -1,12 +1,12 @@
-const sass = require('node-sass');
-const fs = require('fs');
-const path = require('path');
+const sass = require("node-sass");
+const fs = require("fs");
+const path = require("path");
 
 try {
-  const inputPath = path.join('scss', 'logi-bootstrap.scss');
-  const inputFontPath = path.join('./', 'fonts');
-  const outputPath = path.join('dist', 'logi-bootstrap.css');
-  const outputFontPath = path.join('dist', 'fonts');
+  const inputPath = path.join("scss", "logi-bootstrap.scss");
+  const inputFontPath = path.join("./", "fonts");
+  const outputPath = path.join("dist", "logi-bootstrap.css");
+  const outputFontPath = path.join("dist", "fonts");
 
   const { css } = sass.renderSync({
     file: inputPath,
@@ -17,14 +17,13 @@ try {
 
   // Copy fonts
   fs.readdirSync(inputFontPath)
-    .filter(f => !fs.statSync(`${inputFontPath}/${f}`).isDirectory())
+    .filter((f) => !fs.statSync(`${inputFontPath}/${f}`).isDirectory())
     .forEach((f) => {
-    fs.copyFileSync(`${inputFontPath}/${f}`, path.join(outputFontPath, f));
-  });
+      fs.copyFileSync(`${inputFontPath}/${f}`, path.join(outputFontPath, f));
+    });
 
-  console.log('OK!');
+  console.log("OK!");
 } catch (error) {
-  console.error('An error occurred!');
+  console.error("An error occurred!");
   console.error(error);
 }
-
